b76a75c0e4ea9b156a64ca555862cc941baeb0d2
LinusMolin98 <>
2024-01-02 22:17:47 +0000

    added inital javaScript to get a solid start

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+function checkAnswers() {
+    let correctAnswers = 0;
+
+    
+    if (document.querySelector('input[name="q1"]:checked').value === "a") {
+        correctAnswers++;
+    }
+
+    
+    if (document.querySelector('input[name="q2"]:checked').value === "a") {
+        correctAnswers++;
+    }
+
+    
+    if (document.querySelector('input[name="q3"]:checked').value === "b") {
+        correctAnswers++;
+    }
+
+   
+    document.getElementById('result').innerHTML = "You got " + correctAnswers + " out of 3 questions correct!";
+}
